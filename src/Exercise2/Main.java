df03bcefcf94270185607476a6e019a31da7f68c
SaulSilver <hatem.houssein2gmail.com>
2016-10-07 17:31:43 +0200

    quicksort is done

--- src/Exercise2/Main.java
+++ src/Exercise2/Main.java
@@ -41,6 +41,8 @@ public class Main implements A2Main {
     @Override
     public long bubbleSortByTransactionValue(List<Item> array) {
         long start = System.nanoTime();
+        if (array == null || array.size() == 0)
+            return 0;
         int j;
         boolean flag = true;        //Set flag to true to begin first pass
         Item temp;
@@ -51,7 +53,7 @@ public class Main implements A2Main {
             for (j = 0; j < array.size()-1; j++) {
                 double value1 = array.get(j).getTransactionValue();
                 double value2 = array.get(j+1).getTransactionValue();
-                if (value1 > value2) {
+                if (value1 < value2) {
                     //Swap elements
                     temp = new Item(array.get(j).getPerformer(), value1, array.get(j).getDate());
                     array.set(j, array.get(j+1));
@@ -66,7 +68,38 @@ public class Main implements A2Main {
     /** Sort array by transaction value with quick sort algorithm */
     @Override
     public long quickSortByTransactionValue(List<Item> array) {
-        return 0;
+        long start = System.nanoTime();
+        if (array == null || array.size() == 0)
+            return 0;
+        quicksort(0, array.size() - 1, array);
+        return System.nanoTime() - start;
+    }
+
+    private void quicksort(int low, int high, List<Item> array) {
+        int i = low;
+        int j = high;
+        double pivot = array.get(low + (high - low) / 2).getTransactionValue();
+
+        while (i <= j) {
+            while (array.get(i).getTransactionValue() < pivot)
+                i++;
+            while (array.get(j).getTransactionValue() > pivot)
+                j--;
+            if (i <= j) {
+                //Exchange values
+                Item temp = array.get(i);
+                array.set(i, array.get(j));
+                array.set(j, temp);
+
+                i++;
+                j--;
+            }
+        }
+        //recursion step
+        if (low < j)
+            quicksort(low, j, array);
+        if (i < high)
+            quicksort(i, high, array);
     }
 
     /** Sort array by transaction value with merging sort algorithm, using priority queue and comparator */
--- src/test.java
+++ src/test.java
@@ -4,13 +4,14 @@ import Exercise2.Main;
 import java.util.List;
 
 /**
+ *
  * Created by hatem on 2016-10-07.
  */
 public class test {
     public static void main(String[] args) {
         Main main = new Main();
         List<Item> items = main.readCSVFile("/input.txt");
-        long timeSpent = main.bubbleSortByTransactionValue(items);
+        long timeSpent = main.quickSortByTransactionValue(items);
         System.out.println(timeSpent + " ns");
     }
 }
